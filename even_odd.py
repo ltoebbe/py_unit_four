f01c3f16730074123d5664c18fa6d02e21c2112b
ltoebbe <144365202+ltoebbe@users.noreply.github.com>
2023-10-18 09:01:12 -0400

    c==B

--- even_odd.py
+++ even_odd.py
@@ -1,16 +1,17 @@
 
+number = int(input("enter number: "))
+
+
 def even_or_odd(number):
     if number % 2 == 0:
-        return number + "is an even number"
+        return str(number) + " is an even number"
     if number % 2 == 1:
-        return number + "is an odd number"
+        return str(number) + " is an odd number"
 
 
 def main():
-    # First, make sure to delete the word "pass" then get input from the user.
-    # They should type in a number, make sure to convert it to an int
-    # Next, call the even_or_odd function, and make sure to pass the user's number as a parameter.
-    pass
+    even_or_odd(number)
+    print(even_or_odd(number))
 
 
 if __name__ == '__main__':
