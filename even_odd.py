b0cbde9bf1b145b8d008af46a985827fd5a5c7e2
ltoebbe <144365202+ltoebbe@users.noreply.github.com>
2023-10-18 08:49:58 -0400

    c==B

--- even_odd.py
+++ even_odd.py
@@ -1,11 +1,9 @@
 
 def even_or_odd(number):
-    """
-    Create a function that will tell if a number is even or odd. Use two if statements to do this.
-    :param number: could be any positive or negative integer
-    :return: either "x is an even number" or "x is an odd number"
-    """
-    pass
+    if number % 2 == 0:
+        return number + "is an even number"
+    if number % 2 == 1:
+        return number + "is an odd number"
 
 
 def main():
