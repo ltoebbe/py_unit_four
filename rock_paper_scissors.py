ef7a8512cf4a64da112195b3a2f27d687a3d50ff
ltoebbe <144365202+ltoebbe@users.noreply.github.com>
2023-10-25 09:00:14 -0400

    gufwkhbdj

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -1,12 +1,29 @@
-# hello world
-#
+import random
+
+
 def who_wins(user, computer):
-    # 1 is rock, 2 is paper, 3 is scissors
-    pass
+    # 1 is rock, 2 is paper, 3 is scissors, 4 is lizard, 5 is spock
+    rock_victory = rock + scissors
+    rock_victory = rock + lizard
+
+    paper_victory = paper + rock
+    paper_victory = paper + spock
+
+    scissors_victory = scissors + paper
+    scissors_victory = scissors + lizard
+
+    lizard_victory = lizard + paper
+    lizard_victory = lizard + spock
+
+    spock_victory = spock + rock
+    spock_victory = spock + scissors
+
 
 
 def main():
-    pass
+    comp_play = randint(1, 5)
+    user_play = input("choose rock, paper, scissors, lizard, or spock:")
+
 
 
 if __name__ == '__main__':
