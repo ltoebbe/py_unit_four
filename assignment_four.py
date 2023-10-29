d12b251f5a8740665c3ba71f4bd700187c2bca1b
ltoebbe <144365202+ltoebbe@users.noreply.github.com>
2023-10-29 19:13:47 -0400

    (-̥̥̥̥̥̥̥̥̥̥̥̥̥̥̥̥̥̥̥̥̥̥̥̥̥᷄_-̥̥̥̥̥̥̥̥̥̥̥̥̥̥̥̥̥̥̥̥̥̥̥̥̥᷅ )

--- assignment_four.py
+++ assignment_four.py
@@ -24,3 +24,39 @@ import random
 # print results
 # ask user if they want to draw again
 #   if yes, continue. if no, run get_winner and display results
+
+
+def get_card():
+    return random.randint(1, 10)
+
+
+def user_total():
+    u_card1 = get_card()
+    print("you drew a ", u_card1)
+    print("your total is now ", u_current + u_card1)
+
+
+def dealer_total():
+    d_card1 = get_card()
+    print("I drew a ", d_card1)
+    print("my total is now ", d_current + d_card1)
+
+
+def get_winner(user_total, dealer_total):
+    if user_total > 21 and dealer_total > 21:
+        print("both lose!")
+    elif user_total > 21 and dealer_total <= 21:
+        print("I win "
+              "gg ez")
+    elif user_total <= 21 and dealer_total > 21:
+        print("you win!")
+    elif user_total < 21 and dealer_total < 21:
+        input("the scores are ", user_total, dealer_total, ", would you like to draw another card? type 'y' for yes, 'n' for no.")
+            if input = y:
+                get_card()
+            if input = n:
+                get_winner(user_total, dealer_total)
+
+# i'm gonna have a freaking conniption
+# i can't make it work
+# (-̥̥̥̥̥̥̥̥̥̥̥̥̥̥̥̥̥̥̥̥̥̥̥̥̥᷄_-̥̥̥̥̥̥̥̥̥̥̥̥̥̥̥̥̥̥̥̥̥̥̥̥̥᷅ )
